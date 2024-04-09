0ebb15155aec317d60c5a279bd8924f59659a1df
jamalmuradagaev <jamalmuradagaev@yandex.ru>
2024-04-09 23:14:29 +0300

    реализована возможность добавить новый продукт  (данные изменяются не только на самой странице но и на сервере)

--- src/App.jsx
+++ src/App.jsx
@@ -22,12 +22,76 @@ function App() {
 
     getProducts()
   }, [])
+
+  // функция изменения продукта
+  const handleEditProductSubmit  = async(e) => {
+    e.preventDefault()
+  }
+
+  // функция добавления нового продукта
+  const handleAddProductSubmit  = async(e) => {
+    e.preventDefault()
+
+    const newProduct = {
+      title: e.target.title.value,
+      description: e.target.description.value,
+      liked: false,
+    }
+
+    const response = await fetch(URL, {
+      method: "POST",
+      body: JSON.stringify(newProduct),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) return;
+
+    const createdProduct = await response.json()
+
+    setProducts([...products, createdProduct]);
+
+    e.target.title.value = ''
+    e.target.description.value = ''
+
+    return true
+  }
   
 
   return (
     <div className='app'>
       <h1>Products</h1>
 
+      <div className='forms'>
+
+        {/* Форма изменеиня данных */}
+        <form action="" onSubmit={handleEditProductSubmit }>
+          <h2>Edit</h2>
+          <div>
+            <input type="text" name='title'/> 
+          </div>
+          <div>
+            <textarea name="description" id="" cols="30" rows="10"></textarea>
+          </div>
+          <button>Submit</button>
+        </form>
+
+        {/* Форма добавления данных */}
+        <form action="" onSubmit={handleAddProductSubmit }>
+          <h2>Add</h2>
+          <div>
+            <input type="text" name='title'/> 
+          </div>
+          <div>
+            <textarea name="description" id="" cols="30" rows="10"></textarea>
+          </div>
+          <button>Submit</button>
+        </form>
+
+      </div>
+
+      {/* вывод данных */}
       {products.map(product => {
         return (
           <div key={product.id} className='product'>
